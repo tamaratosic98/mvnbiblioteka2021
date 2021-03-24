54d48c3e20bd480c1ff7d35ef82c4b353eadc5ab
tamaratosic98 <tosict98@gmail.com>
2021-03-24 23:07:47 +0100

    Dodate test klase za svaku klasu iz projekta. Izgenerisan javaDOC.

--- src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/Knjiga.java
+++ src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/Knjiga.java
@@ -1,49 +1,120 @@
 package rs.ac.bg.fon.nprog.mvnbiblioteka2021.biblioteka;
 
 import java.util.LinkedList;
-
+/**
+ * Klasa koja predstavlja Knjigu.
+ * 
+ * Knjiga ima naslov, isbn, izdavaca kao String vrednosti, listu Autora i izdanje kao int vrednost.
+ * @author Tamara Tosic
+ * @version 1.0
+ *
+ */
 public class Knjiga {
+	/**
+	 * Naslov knjige kao String vrednost.
+	 */
 	private String naslov;
+	/**
+	 * Lista autora knjige tipa klasa Autor.
+	 */
 	private LinkedList<Autor> autori;
+	/**
+	 * ISBN knjige kao String vrednost.
+	 */
 	private String isbn;
+	/**
+	 * Izdavac knjige kao String vrednost.
+	 */
 	private String izdavac;
+	/**
+	 * Izdanje knjige kao int vrednost.
+	 */
 	private int izdanje;
-	
+	/**
+	 * @return Naslov knjige kao String vrednost.
+	 */
 	public String getNaslov() {
 		return naslov;
 	}
+	/**
+	 * Postavlja naslov knjige na novu vrednost. 
+	 * 
+	 * @param naslov Naslov knjige kao String vrednost.
+	 */
 	public void setNaslov(String naslov) {
 		this.naslov = naslov;
 	}
+	/**
+	 * 
+	 * @return Lista autora knjige tipa klasa Autor.
+	 */
 	public LinkedList<Autor> getAutori() {
 		return autori;
 	}
+	/**
+	 * Postavlja naslov knjige na novu vrednost.
+	 * 
+	 * @param autori Autori kao Lista autora knjige tipa klase Autor.
+	 */
 	public void setAutori(LinkedList<Autor> autori) {
 		this.autori = autori;
 	}
+	/**
+	 * 
+	 * @return ISBN knjige kao String vrednost.
+	 */
 	public String getIsbn() {
 		return isbn;
 	}
+	/**
+	 * Postavlja isbn na novu vrednost.
+	 * 
+	 * @param isbn ISBN knjige kao String vrednost.
+	 */
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+	/**
+	 * 
+	 * @return Izdavac knjige kao String vrednost.
+	 */
 	public String getIzdavac() {
 		return izdavac;
 	}
+	/**
+	 * Postavlja izdavaca knjige na novu vrednost.
+	 * 
+	 * @param izdavac Izdavac knjige kao String vrednost.
+	 */
 	public void setIzdavac(String izdavac) {
 		this.izdavac = izdavac;
 	}
+	/**
+	 * 
+	 * @return Izdanje knjige kao int vrednost.
+	 */
 	public int getIzdanje() {
 		return izdanje;
 	}
+	/**
+	 * Postavlja izdanje knjige na novu vrednost.
+	 * 
+	 * @param izdanje Izdanje knjige kao int vrednost.
+	 */
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
+	/**
+	 * @return Vraca String sa svim podacima o knjizi.
+	 */
 	@Override
 	public String toString() {
 		return "Knjiga [naslov=" + naslov + ", autori=" + autori + ", isbn=" + isbn + ", izdavac=" + izdavac
 				+ ", izdanje=" + izdanje + "]";
 	}
+	/**
+	 * 
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -51,6 +122,19 @@ public class Knjiga {
 		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
 		return result;
 	}
+	/**
+	 * 
+	 * Poredi dve knjige i vraca true ako su iste, a false ako nisu.
+	 * 
+	 * Knjige se porede po isbn-u.
+	 * 
+	 * @return 
+	 * <ul>
+	 * 		<li>True ako su oba objekta klase Knjiga i imaju isti isbn.</li>
+	 * 		<li>False u svim ostalim slucajevima.</li>
+	 * </ul>
+	 * 
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
--- src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/interfejs/BibliotekaInterface.java
+++ src/main/java/rs/ac/bg/fon/nprog/mvnbiblioteka2021/biblioteka/interfejs/BibliotekaInterface.java
@@ -20,9 +20,29 @@ public interface BibliotekaInterface {
 	 * @throws java.lang.RuntimeException Ako uneta knjiga vec postoji u biblioteci (duplikat).
 	 * 
 	 */
+	/**
+	 * 
+	 * @param knjiga Dodaje novu knjigu.
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
+	/**
+	 * 
+	 * @param knjiga Brise knjigu.
+	 */
 	public void obrisiKnjigu(Knjiga knjiga);
+	/**
+	 * 
+	 * @return Lista svih knjiga.
+	 */
 	public LinkedList<Knjiga> vratiSveKnjige();
+	/**
+	 * 
+	 * @param autor Autor knjige tipa klase Autor.
+	 * @param isbn ISBN knjige tipa int.
+	 * @param naslov Naslov knjige tipa String.
+	 * @param izdavac Izdavac knjige tipa String.
+	 * @return Lista svih knjiga koje zadovoljavaju kriterijum pretrage. 
+	 */
 	public LinkedList<Knjiga> pronadjiKnjigu(
 			Autor autor,
 			String isbn,
